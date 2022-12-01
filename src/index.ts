a2e964f18d314881018a196808e287101f266197
Sander van den Akker <sander@sandervandenakker.nl>
2022-12-01 18:57:40 +0100

    Add FinalizationRegistry stub

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
 import Actions from '@probot/adapter-github-actions';
 import app from './app';
 
+global.FinalizationRegistry =
+  global.FinalizationRegistry ||
+  class FakeFinalizationRegistry {
+    register() {}
+
+    unregister() {}
+  };
+
 Actions.run(app);
